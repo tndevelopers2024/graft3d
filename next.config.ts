6bef4627804a5e4556353b999220d110eff5e983
madhavan-developer <madhavangopideveloper@gmail.com>
2026-06-10 16:06:31 +0530

    remove www form the url

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.graft3d.com',
+          },
+        ],
+        destination: 'https://graft3d.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
